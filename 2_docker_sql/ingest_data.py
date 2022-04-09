263a414bc274c71a767ce3edd91b9914736e1997
Luis <lfponton@gmail.com>
2022-04-09 21:35:34 +0200

    python file to ingest data

---
+++ 2_docker_sql/ingest_data.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import os
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv'
+
+    # Downlod csv file
+    os.system(f"wget {url} -O {csv_name}")
+    
+    # Connect to the database using a database engine
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    # Load file in chunks of 100000 rows by using the iterator
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    # Change dates from text to timestamp.
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    # Take only the headers of the first dataframe and add them to the schema so that we can insert all the data.
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    # Use the iterator to load all the data to the database in chunks.
+    while True:
+        t_start = time()
+        
+        df = next(df_iter)
+        
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name='table_name', con=engine, if_exists='append')
+        
+        t_end = time()
+        
+        print('inserted another chunk..., it took %.3f seconds' % (t_end - t_start))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres.')
+
+    parser.add_argument('user', help='user name for postgres')
+    parser.add_argument('password', help='password for postgres')
+    parser.add_argument('host', help='host for postgres')
+    parser.add_argument('port', help='port for postgres')
+    parser.add_argument('db', help='database name for postgres')
+    parser.add_argument('table-name', help='name of the table where the results are written')
+    parser.add_argument('url', help='url of the csv file')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+    main(args)
+
+
